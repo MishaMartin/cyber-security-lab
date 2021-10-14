79bec73d43c084cf5d796c3382788de473fd3f30
Misha Martin <mishanicolemartin@gmail.com>
2021-10-14 15:30:36 -0600

    Adding commits

--- part-1/server/controllers/auth.js
+++ part-1/server/controllers/auth.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs')
 const users = []
 
 module.exports = {
@@ -6,16 +7,39 @@ module.exports = {
       console.log(req.body)
       const { username, password } = req.body
       for (let i = 0; i < users.length; i++) {
-        if (users[i].username === username && users[i].password === password) {
-          res.status(200).send(users[i])
+        if (users[i].username === username){
+          let existing = bcrypt.compareSync(password, users[i].pinHash)
+          if(existing){
+            let returnNewUsers = {...users[i]}
+            delete returnNewUsers.pinHash
+            res.status(200).send(returnNewUsers)
+            return
+          }
         }
       }
       res.status(400).send("User not found.")
     },
     register: (req, res) => {
-        console.log('Registering User')
-        console.log(req.body)
-        users.push(req.body)
-        res.status(200).send(req.body)
+        // console.log('Registering User')
+        // console.log(req.body)
+        const {username, email, firstName, lastName, password, password2} = req.body
+        let salt = bcrypt.genSaltSync(5)
+        let pinHash = bcrypt.hashSync(password, salt)
+        
+
+        const newUsers = {
+          username,
+          email,
+          firstName,
+          lastName,
+          pinHash,
+      }
+
+        users.push(newUsers)
+
+        let returnNewUsers= {...newUsers}
+        delete returnNewUsers.pinHash
+
+        res.status(200).send(returnNewUsers)
     }
 }
